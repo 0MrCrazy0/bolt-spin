806822478394d6d59140769e050f01e7687b9d75
MRCRAZY <102135865+0MrCrazy0@users.noreply.github.com>
2026-09-13 08:41:03 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bolt-spin-v7.1.1';
+const CACHE_NAME = 'bolt-spin-v7.1.2';
 const ASSETS = [
   './','./index.html','./manifest.json',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png',
